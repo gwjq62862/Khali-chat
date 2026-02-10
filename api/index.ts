d2b94114833a217799c3978ebb9ce7957e9d7900
Khali <min427188@gmail.com>
2026-02-10 21:10:12 +0630

    feat: implement socket.io server logic and message rooms

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,16 @@
 import { app, PORT } from "./src/app";
 import { connectToDatabase } from "./src/config/database.config";
+import { createServer } from "http";
+import { initializeSocket } from "./src/utils/socket";
+
+const httpServer=createServer(app)
 
 
-
-
-
-
+initializeSocket(httpServer)
 
 const startServer = async () => {
     await connectToDatabase();
-    app.listen(PORT, () => {
+   httpServer.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 };
